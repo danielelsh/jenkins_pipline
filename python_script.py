535c9c926d52a2be766c256073af09545d08c8fe
Daniel Shemesh <daniel.shemesh@argus-sec.com>
2024-04-11 15:19:10 +0300

    test

--- python_script.py
+++ python_script.py
@@ -2,17 +2,28 @@ import datetime
 import platform
 import tempfile
 import os
+import boto3
+
 
 current_datetime = datetime.datetime.now()
 formatted_datetime = current_datetime.strftime("%Y-%m-%d_%H.%M.%S")
 temp_folder = tempfile.gettempdir()
 os_name = platform.system()
 os_version = platform.version()
-
 fp = os.path.join(temp_folder, f"{formatted_datetime}_artifact.txt")
+
+
+def upload_file_to_s3(file_path):
+    s3 = boto3.client('s3')
+    try:
+        s3.upload_file(file_path, "danielshemesh", f"jenkins_artifacts/{formatted_datetime}_artifact.txt")
+    except Exception as e:
+        print("Error uploading file to S3:", e)
+
+
 with open(fp, "w") as file:
     file.write("Current Date and Time: " + formatted_datetime + "\n")
     file.write("Operating System: " + os_name + "\n")
     file.write("OS Version: " + os_version + "\n")
 
-print(f"artifact path: {fp}")
+upload_file_to_s3(fp)
